a14295aed3ce8d7d9121cbcdaef20bd4cdd04eff
Ivan K <divout@gmail.com>
2014-01-12 19:46:34 +0100

    #4 add more columns to select list of variant_images

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,3 +1,3 @@
 Spree::Product.class_eval do
-  has_many :variant_images, -> { order(:position) }, source: :images, through: :variants_including_master
+  has_many :variant_images, source: :images, through: :variants_including_master, order: 'spree_assets.position', select: 'spree_assets.*, spree_assets.position, spree_variants.position', uniq: true
 end
