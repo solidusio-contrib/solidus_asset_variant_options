d8672bf0e71b5da4e577130c3d55be3cb0a45ed9
John Hawthorn <john@stembolt.com>
2017-06-13 13:17:37 -0700

    Avoid redefining Variant#images association
    Rails 5.1 doesn't seem to allow redefining associations (which is fair
    enough) this works around the issue by using a different name and
    aliasing the existing accessor methods.

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,4 +1,7 @@
 Spree::Variant.class_eval do
   has_many :variant_images, -> { order(:position) }, class_name: '::Spree::VariantImage'
-  has_many :images, through: :variant_images, source: :image
+  has_many :variant_image_images, through: :variant_images, source: :image
+
+  alias_method :images, :variant_image_images
+  alias_method :images=, :variant_image_images=
 end
