76cf5a91e9f25076959c4fee98fc7e17f4ad78ea
Jordan Byron <jordan.byron@gmail.com>
2017-09-19 15:28:01 -0400

    If viewable_ids is blank fall back to viewable_id
    This allows the existing drag / drop image upload form to continue to
    function with no additional modifications

--- app/controllers/spree/admin/images_controller_decorator.rb
+++ app/controllers/spree/admin/images_controller_decorator.rb
@@ -9,6 +9,15 @@ Spree::Admin::ImagesController.class_eval do
     end
 
     def viewable_ids
+      # If viewable_ids is blank fall back to viewable_id
+      #
+      # This allows the existing drag / drop image upload form to continue to
+      # function with no additional modifications
+      #
+      if params[:image][:viewable_ids].nil? && params[:image][:viewable_id].present?
+        params[:image][:viewable_ids] = [params[:image][:viewable_id]]
+      end
+
       params[:image][:viewable_ids].reject(&:blank?)
     end
 end
