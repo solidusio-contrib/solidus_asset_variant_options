38115151ba554f7dd00eff89d67a15448d2775a3
Angel Perez <iAngel.p93@gmail.com>
2019-04-19 09:33:06 -0400

    Use rails_helper from Solidus Support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 # Run Coverage report
 require "simplecov"
+
 SimpleCov.start do
   add_filter "spec/dummy"
   add_group "Controllers", "app/controllers"
@@ -14,42 +15,23 @@ end
 ENV["RAILS_ENV"] = "test"
 
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
-
-require "rspec/rails"
-
-require "database_cleaner"
-require "ffaker"
-
-require "spree/testing_support/factories"
+require 'solidus_support/extension/rails_helper'
 require "spree_asset_variant_options/factories"
 
 Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
-  config.mock_with :rspec
-
-  config.color = true
-  config.order = "random"
-
   config.expose_current_running_example_as :example
-  config.fail_fast = ENV["FAIL_FAST"] || false
-
-  config.filter_run focus: true
-  config.run_all_when_everything_filtered = true
   config.use_transactional_fixtures = false
-
-  config.include FactoryBot::Syntax::Methods
 
   # Ensure Suite is set to use transactions for speed.
   config.before :suite do
     DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with :truncation
   end
 
   # Before each spec check if it is a Javascript test and switch between using database transactions or not where necessary.
   config.before :each do |example|
-    DatabaseCleaner.strategy = example.metadata[:js] ? :truncation : :transaction
     DatabaseCleaner.start
   end
 
