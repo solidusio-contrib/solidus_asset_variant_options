07ee284f9987b2a68eac61b036cd6ffecd2001ae
Stefan <stefan@yo-code.de>
2020-06-28 22:44:23 +0200

    always use master variant if no variants given; simplify override

--- app/decorators/controllers/spree/admin/images_controller_decorator.rb
+++ app/decorators/controllers/spree/admin/images_controller_decorator.rb
@@ -10,11 +10,17 @@ module Spree
       private
 
       def set_variants
-        @image.variant_ids = viewable_ids
+        if params[:image][:viewable_ids].present? &&
+           params[:image][:viewable_ids].reject(&:blank?).present?
+          @image.variant_ids = params[:image][:viewable_ids].reject(&:blank?)
+        else #use master variant as falback when empty
+          @image.variant_ids = [@product.master.id]
+        end
       end
 
-      def viewable_ids
-        params[:image][:viewable_ids].reject(&:blank?)
+      def set_viewable # viewable remains first variant
+        @image.viewable_type = 'Spree::Variant'
+        @image.viewable_id = @image.variant_ids.first
       end
 
       ::Spree::Admin::ImagesController.prepend self
